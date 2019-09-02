b527718509a5ef539432c66ab189457b0e2cd8d0
Przemek <kopka.przemyslaw@gmail.com>
2019-09-02 09:09:02 +0200

    template generator for ToF

--- template_generator/template_generator.py
+++ template_generator/template_generator.py
@@ -1,16 +1,26 @@
 from jinja2 import Template
 from math import atan,ceil, degrees, radians, tan
 # settings
-number_of_rings = 500
-lenght = 200.0  # in cm
-inner_ring_diameter = 85.56   # in cm
-average_depth_of_interaction = 0.95  # in cm
+number_of_rings = 100#400
+lenght = 50.0  # in cm
+inner_ring_diameter = 85.56 # 43.56*2  # in cm
+average_depth_of_interaction = 0.95#1.0  # in cm
 number_of_detector = 384
-FOV = 200# in cm
+FOV = 50.0# in cm
+
+# TOF parameters
+tof=True
+number_of_tof_bins = 410
+size_of_time_bin = 10 # ps
+time_resolution = 400 # ps
+tof_mashing_factor = 82
+#numer of TOF position = number_of_tof_bins / tof_mashing_facto
+
+
 
 
 # outputs file name
-sinogram_file_name = "nema_quality.hs"
+sinogram_file_name = "scanner_template.hs"
 root_header_file_name = "root_header.hroot"
 
 
@@ -28,15 +38,21 @@ size.extend(list(range(number_of_rings - 1, number_of_rings - FOV_range, -1)))
 diff = list(range(-FOV_range + 1, 1))
 diff.extend(list(range(1, FOV_range)))
 
+angle= degrees(atan(FOV/85.56/2.0))
+FOV_1 = tan(radians(int(angle)))*(inner_ring_diameter/2.0)
+FOV_2 = tan(radians(ceil(angle)))*(inner_ring_diameter/2.0)
+FOV1_range = int(int(FOV_1 / lenght * number_of_rings))
+FOV2_range = int(int(FOV_2 / lenght * number_of_rings))
+# angle= degrees(atan(FOV/85.56/2.0))
 
-
+print(angle, int(angle), ceil(angle), FOV1_range, FOV2_range, (number_of_rings-FOV1_range+1, number_of_rings-FOV2_range+1))
 
 def list2str(_list):
     """convert list to string fotmat list in STIR"""
     return str(_list).replace(" ", "").replace('[', '{ ').replace(']', ' }')
 
 
-with open('nema.template') as file_:
+with open('scanner_template.template') as file_:
     template = Template(file_.read())
 
 with open('root_header.template') as file_:
@@ -52,7 +68,12 @@ output = template.render(number_of_rings=number_of_rings, diff=list2str(diff), s
                          inner_ring_diameter=inner_ring_diameter,
                          non_arc_corrected_bins=non_arc_corrected_bins,
                          default_bin_size=default_bin_size,
-                         number_of_detector=number_of_detector
+                         number_of_detector=number_of_detector,
+                         tof=tof,
+                         number_of_tof_bins=number_of_tof_bins,
+                         size_of_time_bin=size_of_time_bin,
+                         time_resolution=time_resolution,
+                         tof_mashing_factor=tof_mashing_factor,
                          )
 
 text_file = open(sinogram_file_name, "w")
@@ -63,7 +84,13 @@ output_root = template_root.render(number_of_rings=number_of_rings, dist=dist,
                                    default_bin_size=default_bin_size,
                                    number_of_detector=number_of_detector,
                                    inner_ring_diameter=inner_ring_diameter,
-                                   average_depth_of_interaction=average_depth_of_interaction, )
+                                   average_depth_of_interaction=average_depth_of_interaction,
+                                   tof=tof,
+                                   number_of_tof_bins=number_of_tof_bins,
+                                   size_of_time_bin=size_of_time_bin,
+                                   time_resolution=time_resolution,
+                                   tof_mashing_factor=tof_mashing_factor,
+                                   )
 
 text_file2 = open("root_header.hroot", "w")
 text_file2.write(output_root)
